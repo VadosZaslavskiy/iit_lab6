4c5b0df73ef1e36113d05817daf47dd4f0691a61
VadosZaslavskiy <113304153+VadosZaslavskiy@users.noreply.github.com>
2024-05-27 18:27:52 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -63,7 +63,7 @@ resource "aws_instance" "docker_instance" {
     sudo apt-get install -y nginx
     sudo systemctl start nginx
     sudo systemctl enable nginx
-    echo "<h3>Zaslavskiy Vadim</h3><h3>Zibnitskiy Danila</h3><b>USED VOLUME</b>" > /var/www/html/index.html
+    echo "<h3>Brigada 2</h3><h3>Terraform</h3>" > /var/www/html/index.html
   EOF
 
   tags = {
