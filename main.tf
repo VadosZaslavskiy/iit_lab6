c5ae5fec013905f656a4dcdf0424c8dedcf907dc
VadosZaslavskiy <113304153+VadosZaslavskiy@users.noreply.github.com>
2024-05-27 17:24:54 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -53,7 +53,7 @@ resource "aws_security_group" "docker_sg" {
 
 resource "aws_instance" "docker_instance" {
   ami                    = "ami-064087b8d355e9051"
-  instance_type          = "t2.nano"
+  instance_type          = "t3.micro"
   key_name               = "lab"
   vpc_security_group_ids = [aws_security_group.docker_sg.id]
 
